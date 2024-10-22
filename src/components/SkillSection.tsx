3bb8e3896432266c371de3154b9f506ed09535b2
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-10-23 00:18:00 +0500

    replace single quote with their character

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -11,19 +11,19 @@ const SkillsSection = () => {
       title: "Web Development",
       icon: <FaLaptopCode />, // Using react-icons for web development
       description:
-        "As a developer, I find myself most captivated by the power and flexibility of NEXT.js. I'm always eager to dive into new projects that leverage NEXT.js and discover innovative ways to create fast, scalable, and user-friendly applications.",
+        "As a developer, I find myself most captivated by the power and flexibility of NEXT.js. I&apos;m always eager to dive into new projects that leverage NEXT.js and discover innovative ways to create fast, scalable, and user-friendly applications.",
     },
     {
       title: "App Development",
       icon: <FaMobileAlt />, // Using react-icons for app development
       description:
-        "With a focus on user-centric design and cutting-edge technologies, I thrive on building intuitive and efficient apps that make a positive impact on people's lives. Let's turn ideas into reality and shape the future together.",
+        "With a focus on user-centric design and cutting-edge technologies, I thrive on building intuitive and efficient apps that make a positive impact on people&apos;s lives. Let&apos;s turn ideas into reality and shape the future together.",
     },
     {
       title: "UI/UX Designing",
       icon: <FaPalette />, // Using react-icons for UI/UX designing
       description:
-        "Crafting visually appealing and intuitive user interfaces that offer a delightful user experience is something I'm truly fanatic about.",
+        "Crafting visually appealing and intuitive user interfaces that offer a delightful user experience is something I&apos;m truly fanatic about.",
     },
     {
       title: "Mentorship",
